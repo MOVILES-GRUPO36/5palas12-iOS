0509c015b09ff258411fd7a181539fce8bd0c803
sanrappy <111755845+sanrappy@users.noreply.github.com>
2024-09-03 14:03:53 -0500

    Initial Commit

---
+++ 5palas12-iOS/_palas12_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  _palas12_iOSApp.swift
+//  5palas12-iOS
+//
+//  Created by santiago on 03/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct _palas12_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
