c378c9aa905731366168e8963854ec8450dc0422
Santiago Rappy <111755845+sanrappy@users.noreply.github.com>
2024-10-01 18:46:53 -0500

    Update LoginScreen.swift

--- 5palas12-iOS/LoginScreen.swift
+++ 5palas12-iOS/LoginScreen.swift
@@ -137,24 +137,7 @@ struct LoginScreen: View {
                 
                 // Successfully logged in
                 loginResponse = "Login successful!"
-                // You can navigate to another view or perform other actions here
-            }
         }
-}
-
-extension Color {
-    init(hex: String) {
-        let scanner = Scanner(string: hex)
-        _ = scanner.scanString("#")
-        
-        var rgb: UInt64 = 0
-        scanner.scanHexInt64(&rgb)
-        
-        let red = Double((rgb >> 16) & 0xFF) / 255.0
-        let green = Double((rgb >> 8) & 0xFF) / 255.0
-        let blue = Double(rgb & 0xFF) / 255.0
-        
-        self.init(red: red, green: green, blue: blue)
     }
 }
 
