e65579159ce9fcb0a679c0f25d3f9bd5c636f84a
sanrappy <111755845+sanrappy@users.noreply.github.com>
2024-10-02 08:17:02 -0500

    tabbar

--- 5palas12-iOS/LoginScreen.swift
+++ 5palas12-iOS/LoginScreen.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import FirebaseAuth
 
+
 struct LoginScreen: View {
     @State var email: String = ""
     @State var password: String = ""
@@ -141,23 +142,6 @@ struct LoginScreen: View {
             }
         }
 }
-
-extension Color {
-    init(hex: String) {
-        let scanner = Scanner(string: hex)
-        _ = scanner.scanString("#")
-        
-        var rgb: UInt64 = 0
-        scanner.scanHexInt64(&rgb)
-        
-        let red = Double((rgb >> 16) & 0xFF) / 255.0
-        let green = Double((rgb >> 8) & 0xFF) / 255.0
-        let blue = Double(rgb & 0xFF) / 255.0
-        
-        self.init(red: red, green: green, blue: blue)
-    }
-}
-
 
 
 
