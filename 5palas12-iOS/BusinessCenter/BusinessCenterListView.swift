da106e7a60b864147ccc8c0d83803152c0825e13
sanrappy <111755845+sanrappy@users.noreply.github.com>
2024-10-21 12:16:32 -0500

    tidy up code

--- 5palas12-iOS/BusinessCenter/BusinessCenterListView.swift
+++ 5palas12-iOS/BusinessCenter/BusinessCenterListView.swift
@@ -24,22 +24,20 @@ struct BusinessCenterListView: View {
                 LogoView()
                     .padding(.all, 0)
                 
-                Spacer() // Add Spacer to center the text vertically
-                Spacer() // Add Spacer to center the text vertically
-                               
+                Spacer()
+                
                 Text("Welcome to Business Center")
                     .font(.largeTitle)
                     .bold()
-                    .multilineTextAlignment(.center) // Center-align the text
-                    .foregroundColor(.black) // Set text color explicitly
-                    .frame(maxWidth: .infinity) // Make sure the text fills the available width
-                    .padding() // Optional: Add padding around the text
-                               
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.black)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                
                 Spacer()
-                    
+                
                 
                 ZStack {
-                    // Set the background color for the entire view
                     Color(.timberwolf)
                         .ignoresSafeArea()
                         .padding(.top, 0)
@@ -49,64 +47,59 @@ struct BusinessCenterListView: View {
                             HStack {
                                 if let iconName = item.icon {
                                     Image(systemName: iconName)
-                                        .foregroundColor(item.title == "Delete my business" ? .red : .blue) // Red for "Delete account"
+                                        .foregroundColor(item.title == "Delete my business" ? .red : .blue)
                                 }
                                 
                                 Text(item.title)
-                                    .foregroundColor(item.title == "Delete my business" ? .red : .primary) // Red text for "Delete account"
+                                    .foregroundColor(item.title == "Delete my business" ? .red : .primary)
                                 
                                 Spacer()
                                 
                                 switch item.type {
                                 case .toggle(let isOn):
                                     Toggle("", isOn: .constant(isOn))
-                                        .labelsHidden() // Hide the toggle label
+                                        .labelsHidden()
                                 case .navigation:
                                     Image(systemName: "chevron.right")
                                         .foregroundColor(.gray)
                                 case .plain:
-                                    EmptyView() // No action required
+                                    EmptyView()
                                 }
                             }
-                            .padding(12) // Add padding around each item
+                            .padding(12)
                             .background(
-                                RoundedRectangle(cornerRadius: 8) // Add rounded corners
-                                    .fill(Color.white) // Background color for each item
-//                                    .shadow(color: .gray.opacity(0.2), radius: 4, x: 0, y: 2) // Optional: Add shadow
+                                RoundedRectangle(cornerRadius: 8)
+                                    .fill(Color.white)
                             )
-//                            .padding(.vertical, 5) // Add vertical spacing between items
                             .onTapGesture {
                                 if case .navigation = item.type {
-                                    // Handle navigation to another view
                                     print("Navigate to \(item.title)")
                                 }
                             }
                         }
-                        .listRowBackground(Color.clear) // Make the row background transparent
+                        .listRowBackground(Color.clear)
                     }
-                    .listStyle(PlainListStyle()) // Optional: Change list style if needed
-                    .background(Color.clear) // Set List background to clear
-                    //.navigationBarTitleDisplayMode(.inline) // Uncomment if needed
-//                    .padding(.horizontal)
+                    .listStyle(PlainListStyle())
+                    .background(Color.clear)
                 }
             }
             .background(Color.timberwolf)
         }
         .navigationBarBackButtonHidden(true)
-            .overlay(alignment: .topLeading){
-                
-                Button(action: {
-                    self.presentationMode.wrappedValue.dismiss()
-                }) {
-                    HStack {
-                        Image(systemName: "chevron.left")
-                            .foregroundColor(.timberwolf)
-                        Text("Back")
-                            .foregroundColor(.timberwolf)
-                    }
-                }.offset(x: 10,y: 18)
-                
-            }
+        .overlay(alignment: .topLeading){
+            
+            Button(action: {
+                self.presentationMode.wrappedValue.dismiss()
+            }) {
+                HStack {
+                    Image(systemName: "chevron.left")
+                        .foregroundColor(.timberwolf)
+                    Text("Back")
+                        .foregroundColor(.timberwolf)
+                }
+            }.offset(x: 10,y: 18)
+            
+        }
     }
 }
 
