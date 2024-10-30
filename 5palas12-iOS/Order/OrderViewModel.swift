de5bb50d1ea8734288f0a17918ecae015b3b6f5e
Sebastian <130873171+CodeGao21@users.noreply.github.com>
2024-10-30 12:01:21 -0500

    Update OrderViewModel.swift

--- 5palas12-iOS/Order/OrderViewModel.swift
+++ 5palas12-iOS/Order/OrderViewModel.swift
@@ -1,45 +1,49 @@
 import Foundation
+import Combine
 
 class OrderViewModel: ObservableObject {
-    
     @Published var orders: [OrderModel] = []
     @Published var errorMessage: String? = nil
     @Published var isOrderCreated: Bool = false
 
     private let orderDAO = OrderDAO()
-    private var profileId: String
-    
-    // Inicialización con el ID del perfil del usuario
+    private let profileId: String
+
+    // Initialization with the user's profile ID
     init(profileId: String) {
         self.profileId = profileId
         fetchOrdersByUser()
     }
 
-    // Obtener órdenes por usuario
+    // Fetch orders by user
     func fetchOrdersByUser() {
         orderDAO.getOrdersByUser(profileId: profileId) { [weak self] result in
-            switch result {
-            case .success(let orders):
-                self?.orders = orders
-                print("Orders fetched successfully")
-            case .failure(let error):
-                self?.errorMessage = "Error loading the orders: \(error.localizedDescription)"
-                print(self?.errorMessage ?? "Error ")
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let orders):
+                    self?.orders = orders
+                    print("Orders fetched successfully")
+                case .failure(let error):
+                    self?.errorMessage = "Error loading the orders: \(error.localizedDescription)"
+                    print(self?.errorMessage ?? "Error")
+                }
             }
         }
     }
 
-    // Crear una nueva orden
+    // Create a new order
     func createOrder(order: OrderModel) {
         orderDAO.createOrder(order: order) { [weak self] result in
-            switch result {
-            case .success:
-                self?.isOrderCreated = true
-                print("Order created successfully")
-                self?.fetchOrdersByUser()  // Recargar órdenes después de crear una nueva
-            case .failure(let error):
-                self?.errorMessage = "Error creating the order: \(error.localizedDescription)"
-                print(self?.errorMessage ?? "Error desconocido")
+            DispatchQueue.main.async {
+                switch result {
+                case .success:
+                    self?.isOrderCreated = true
+                    print("Order created successfully")
+                    self?.fetchOrdersByUser()  // Reload orders after creating a new one
+                case .failure(let error):
+                    self?.errorMessage = "Error creating the order: \(error.localizedDescription)"
+                    print(self?.errorMessage ?? "Unknown error")
+                }
             }
         }
     }
