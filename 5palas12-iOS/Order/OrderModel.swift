3f8b6029d775b085ddc5ba40b16d21947d7f7f72
Sebastian <130873171+CodeGao21@users.noreply.github.com>
2024-11-01 15:13:50 -0500

    Update OrderModel.swift

--- 5palas12-iOS/Order/OrderModel.swift
+++ 5palas12-iOS/Order/OrderModel.swift
@@ -1,47 +1,22 @@
 import Foundation
-import FirebaseFirestore
-//import FirebaseFirestoreSwift
 
-class OrderDAO {
+struct OrderModel: Decodable, Identifiable, Equatable {
+    var id = UUID()
+    var userEmail= String
+    var products : [String]
+    var price: Double
+    var isActive : Bool
+    var pickUpTime : String
     
-    private let db = FirestoreManager.shared.db
-    private let collectionName: String = "orders"
-
-    // Obtener órdenes filtradas por usuario (profile_id)
-    func getOrdersByUser(profileId: String, completion: @escaping (Result<[OrderModel], Error>) -> Void) {
-        db.collection(collectionName)
-            .whereField("profile_id", isEqualTo: profileId)  // Filtrar por profile_id
-            .getDocuments { snapshot, error in
-                if let error = error {
-                    completion(.failure(error))
-                } else {
-                    var orders: [OrderModel] = []
-                    for document in snapshot!.documents {
-                        do {
-                            let order = try document.data(as: OrderModel.self)
-                            orders.append(order)
-                        } catch {
-                            completion(.failure(error))
-                            return
-                        }
-                    }
-                    completion(.success(orders))
-                }
-            }
-    }
-
-    // Crear una nueva orden
-    func createOrder(order: OrderModel, completion: @escaping (Result<Void, Error>) -> Void) {
-        do {
-            try db.collection(collectionName).document(order.order_id).setData(from: order) { error in
-                if let error = error {
-                    completion(.failure(error))
-                } else {
-                    completion(.success(()))
-                }
-            }
-        } catch {
-            completion(.failure(error))
-        }
+    enum CodingKeys : String, CodingKey{
+        case userEmail
+        case products
+        case price
+        case isActive
+        case pickUpTime
     }
 }
+
+}
+
+
