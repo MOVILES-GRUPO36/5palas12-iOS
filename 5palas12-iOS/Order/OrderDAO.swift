c1c8f8620736e547a9f3b6d81cb0705908eddf1e
Sebastian <130873171+CodeGao21@users.noreply.github.com>
2024-11-01 15:05:43 -0500

    Update OrderDAO.swift

--- 5palas12-iOS/Order/OrderDAO.swift
+++ 5palas12-iOS/Order/OrderDAO.swift
@@ -1,48 +1,45 @@
-import Foundation
 import FirebaseFirestore
-//import FirebaseFirestoreSwift
-
 
 class OrderDAO {
-    
     private let db = FirestoreManager.shared.db
-    private let collectionName: String = "orders"
-
-    // Obtener órdenes filtradas por usuario (profile_id)
-    func getOrdersByUser(profileId: String, completion: @escaping (Result<[OrderModel], Error>) -> Void) {
-        db.collection(collectionName)
-            .whereField("profile_id", isEqualTo: profileId)  // Filtrar por profile_id
-            .getDocuments { snapshot, error in
-                if let error = error {
-                    completion(.failure(error))
-                } else {
-                    var orders: [OrderModel] = []
-                    for document in snapshot!.documents {
-                        do {
-                            let order = try document.data(as: OrderModel.self)
-                            orders.append(order)
-                        } catch {
-                            completion(.failure(error))
-                            return
-                        }
-                    }
-                    completion(.success(orders))
-                }
+    private let collectionName = "orders"
+    
+    func createOrder(_ order: OrderModel, completion: @escaping (Result<Void, Error>) -> Void) {
+        let orderData: [String: Any] = [
+            "userEmail": order.userEmail,
+            "products": order.products,
+            "price": order.price,
+            "isActive": order.isActive,
+            "pickUpTime": order.pickUpTime
+        ]
+        
+        db.collection(collectionName).addDocument(data: orderData) { error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(()))
             }
+        }
     }
-
-    // Crear una nueva orden
-    func createOrder(order: OrderModel, completion: @escaping (Result<Void, Error>) -> Void) {
-        do {
-            try db.collection(collectionName).document(order.order_id).setData(from: order) { error in
-                if let error = error {
-                    completion(.failure(error))
-                } else {
-                    completion(.success(()))
+    
+    func getAllOrders(byUserEmail email: String, completion: @escaping (Result<[OrderModel], Error>) -> Void) {
+        db.collection(collectionName).whereField("userEmail", isEqualTo: email).getDocuments { snapshot, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                var orders: [OrderModel] = []
+                for document in snapshot!.documents {
+                    do {
+                        let order = try document.data(as: OrderModel.self)
+                        orders.append(order)
+                    } catch {
+                        completion(.failure(error))
+                        return
+                    }
                 }
+                completion(.success(orders))
             }
-        } catch {
-            completion(.failure(error))
         }
     }
 }
+
