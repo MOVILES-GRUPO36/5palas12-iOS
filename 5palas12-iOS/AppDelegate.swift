c9c52668a49092af64783e30657baffe4a60cb98
sanrappy <111755845+sanrappy@users.noreply.github.com>
2024-11-13 20:23:43 -0500

    Rotation fixed to portrait mode
    Log awaited update haha, finally our app will stay in the position it should fixed. :8ball:

--- 5palas12-iOS/AppDelegate.swift
+++ 5palas12-iOS/AppDelegate.swift
@@ -7,11 +7,19 @@
 import FirebaseCore
 import UIKit
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
 
-    return true
-  }
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?
+    ) -> UIInterfaceOrientationMask {
+        return .portrait
+    }
 }
